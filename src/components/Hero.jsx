9b41852fb2518f6badd9cfa2494b7f9724681ef3
Andororo <andororoo@gmail.com>
2023-10-13 18:54:40 -0700

    Demo finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
       <nav className = 'flex justify-between items-center w-full mb-10 pt-3'>
         <img src = {logo} alt='sumz_logo' className='w-28 object-contain'/>
         <button 
-          type='button' 
+          type='button'
           onClick={() => window.open("https://github.com/Andororo")}
           className='black_btn'
         >
@@ -23,7 +23,6 @@ export const Hero = () => {
         Simplify your reading with Summize, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
-
     </header>
   )
 }
